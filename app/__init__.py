63b3c7d8b424bddaf899be4e340518a5a2a9af87
jimjshields <jim.j.shields@gmail.com>
2015-01-21 10:07:27 -0500

    added some db code; running code

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # create flask object
 app = Flask(__name__)
 # config from config module
 app.config.from_object('config')
+# initialize db w/ sqlalchemy
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app import app
+from app import app, db
 
 @app.route('/')
 @app.route('/index')
