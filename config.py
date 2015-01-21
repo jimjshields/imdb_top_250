ca525766b5441f36336722a86e4fe52053083436
jimjshields <jim.j.shields@gmail.com>
2015-01-21 10:08:20 -0500

    git ignore config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# for creating paths
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# path to the db sqlalchemy will use
-SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % (os.path.join(basedir, 'app.db'))
-# path to the repo folder of migrate data files sqlalchemy will use
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
