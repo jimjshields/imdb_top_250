c4865375eece963d5162a9375e53cfc2a1d5f9c4
jimjshields <jim.j.shields@gmail.com>
2015-01-21 17:43:33 -0500

    fixed back to full chart

--- app/static/top_250_track.js
+++ app/static/top_250_track.js
@@ -102,14 +102,10 @@ movie_data.forEach(function(d) {
 	d.date = parseDate(d.date);
 });
 
-// console.log(movie_data)
-
 // nest the data - make the movies the keys in an array of movie objects
 var nest = d3.nest()
 	.key(function(d) { return d.imdb_id; })
 	.entries(movie_data);
-
-console.log(nest)
 
 // add the combo of key and title to an array of arrays - this could definitely be done better
 dataTable = []
@@ -151,7 +147,10 @@ drawFullChart(movie_data, nest, svg, clickedMovies, line, options);
 
 // allow the full chart button to draw it again after being filtered
 var fullChartButton = d3.select("#fullChart")
-fullChartButton.on("click", drawFullChart);
+fullChartButton.on("click", function(d) {
+	console.log("clicked!");
+	drawFullChart(movie_data, nest, svg, clickedMovies, line, options);
+});
 
 // store all of the rows
 var tr = d3.selectAll("tr");
